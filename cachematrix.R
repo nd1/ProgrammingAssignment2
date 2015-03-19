f6e16a8d9f357b34a13a24f80e7401da5389d829
nd1 <nicole@nicoledonnelly.me>
2015-03-18 22:51:06 -0400

    Modified cachematrix.R per the assignment instructions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a cached matrix. Calculate the inverse of the matrix and store it in cache.
 
-## Write a short comment describing this function
+## construct a named list of elements in the environment, for the variable x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invrs <- NULL
+        set <- function(y) {
+                x <<- y
+                invrs <<- NULL
+        }
+        get <- function() x
+        setinvrs <- function(solve) invrs <<- solve
+        getinvrs <- function() invrs
+        list(set = set, get = get,
+             setinvrs = setinvrs,
+             getinvrs = getinvrs)
 }
 
 
-## Write a short comment describing this function
+## calculate the inverse and store as invrs if invrs is null. otherwise, get the cached value of invrs.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invrs <- x$getinvrs()
+        if(!is.null(invrs)) {
+                message("getting cached data")
+                return(invrs)
+        }
+        data <- x$get()
+        invrs <- solve(data, ...)
+        x$setinvrs(invrs)
+        invrs
 }
